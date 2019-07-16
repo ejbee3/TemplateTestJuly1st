a06e4619367def4ba19d95855dd4cfffc7dbcd6a
Joe Baranski <joe.baranski49@gmail.com>
2019-07-16 14:48:28 -0400

    working on splash page

--- ClientApp/src/Pages/Splash.jsx
+++ ClientApp/src/Pages/Splash.jsx
@@ -1,17 +1,36 @@
+import pom from '../images/pom-splash.png'
 import '../scss/Splash.scss'
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Splash() {
+  const [chalkColor, setChalkColor] = useState('')
   return (
-    <div className="main-container">
-      <div className="splash-title">classfetch</div>
-      <img className="splashLogo" alt="some kind of logo" />
-      <p className="description">
-        is a website that allows teachers to keep track of their students'
-        attendance through an interactive user interface. Teachers can keep an
-        eye on previous attendance, excuse absences, and organize class rosters!
-      </p>
+    <div className="big-container">
+      <div className="main-container">
+        <div className="splash-title">
+          classfetch
+          <span>
+            <i class="fas fa-paw" />
+          </span>
+        </div>
+        <img src={pom} className="splashLogo" alt="pom doggo" />
+        <p className="description">
+          is a web app that allows teachers to keep track of their students'
+          attendance through an interactive user interface. Teachers can keep an
+          eye on previous attendance, excuse absences, and organize class
+          rosters!
+        </p>
+        <section className="chalk-container">
+          <article className="chalk" />
+          <article className="chalk" />
+          <article className="chalk" />
+          <article className="chalk" />
+        </section>
+      </div>
+
+      <section className="ledge-container" />
+
       <section className="button-container">
         <Link className="splash-link" to="/signup">
           <button className="splash-button">Register!</button>
